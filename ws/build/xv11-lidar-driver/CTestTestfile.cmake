ccbd1aefccb044fb9904313f826bb3c5244e3bf1
noah epstein <noah@tri.global>
2018-03-05 10:13:09 -0500

    Added XV11 driver to fresh catkin WS

---
+++ ws/build/xv11-lidar-driver/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/noah/projects/nmbot/ws/src/xv11-lidar-driver
+# Build directory: /home/noah/projects/nmbot/ws/build/xv11-lidar-driver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
