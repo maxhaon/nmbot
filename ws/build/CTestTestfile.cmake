b4dd7aee2e95dabf978b12a39737daae2213f90c
noah epstein <noah@tri.global>
2018-03-06 23:01:58 -0500

    Added .gitignore and deleted build and devel files.

--- ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/noah/projects/nmbot/ws/src
-# Build directory: /home/noah/projects/nmbot/ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(xv_11_laser_driver)
--- ws/build/xv_11_laser_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/noah/projects/nmbot/ws/src/xv_11_laser_driver
-# Build directory: /home/noah/projects/nmbot/ws/build/xv_11_laser_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
